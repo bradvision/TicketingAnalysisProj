921ee2f83bf41eea94d83a071424937d09951825
bchoi <rexbradley.c@gmail.com>
2018-10-30 10:41:53 -0400

    updated imports
    line of code for installation of packages

--- Code.R
+++ Code.R
@@ -1,7 +1,13 @@
+install.packages("readr")
+install.packages("VIM")
+install.packages("tidyverse")
+install.packages("data.table")
+
 library(readr)
 library(VIM)
 library(tidyverse)
 library(data.table)
+
 #importing the Traffic_Tickets_Issued__Four_Year_Window
 tdf <- read_csv(file.choose())
 
@@ -14,3 +20,7 @@ vY<- data.table(table(tdf$`Violation Year`))
 vm <- data.table(table(tdf$`Violation Month`))
 sl <-data.table(table(tdf$`State of License`))
 av <- data.table(table(tdf$`Age at Violation`))
+
+
+
+
