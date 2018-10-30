49b98a10c83210cd0427caab8345a9be3f8f0d0d
bchoi <rexbradley.c@gmail.com>
2018-10-30 10:49:46 -0400

    new tables
    Create new tables

--- Code.R
+++ Code.R
@@ -13,6 +13,7 @@ tdf <- read_csv(file.choose())
 
 class(tdf)
 str(tdf)
+colnames(tdf)
 summary(tdf)
 
 #Basic preliminary analysis of the data. 
@@ -20,7 +21,6 @@ vY<- data.table(table(tdf$`Violation Year`))
 vm <- data.table(table(tdf$`Violation Month`))
 sl <-data.table(table(tdf$`State of License`))
 av <- data.table(table(tdf$`Age at Violation`))
-
-
-
-
+G <- data.table(table(tdf$Gender))
+pa <- data.table(table(tdf$`Police Agency`))
+court <- data.table(table(tdf$Court))
